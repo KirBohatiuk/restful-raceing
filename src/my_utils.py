260b9d7ecc924a0c6d66a606471a5b47e2c8d8f9
kir <kirillbogatuk2005@gmail.com>
2023-12-29 20:16:59 +0200

    RESTFULL STUDENT

---
+++ src/my_utils.py
@@ -0,0 +1,5 @@
+
+
+
+
+def stutent_dict():
--- src/view.py
+++ src/view.py
@@ -8,13 +8,15 @@ class StudentInfo(Resource):
         if request.args.get("first_name") and request.args.get("last_name"):
             first_name = request.args.get("first_name")
             last_name = request.args.get("last_name")
-            session = get_session()
             student_dict = {}
+
+            session = get_session()
             students = session.query(StudentModel).filter(StudentModel.first_name == first_name).all()
             for i, student in enumerate(students):
-                student_dict[i] = {"first_name": first_name}
-                student_dict[i] = {"last_name": last_name}
+                student_dict[i] = {"first_name": first_name, "last_name": last_name}
             return student_dict
+        return "Invalid data"
+
     def post(self):
         user_info = request.get_json()
         first_name = user_info["first_name"]
@@ -23,13 +25,26 @@ class StudentInfo(Resource):
         student = create_student(first_name=first_name, last_name=last_name, session=session)
         return student.last_name
 
-    def put(self, first_name, last_name):
-        new_name = request.get_json()
-        student_dict = {}
-        session = get_session()
-        student = session.query(StudentModel).filter(StudentModel.first_name == first_name and StudentModel == last_name)
-        return student
+    def put(self):
+        if request.args.get("first_name") and request.args.get("last_name"):
+            first_name = request.args.get("first_name")
+            last_name = request.args.get("last_name")
+            new_name = request.get_json()
+            session = get_session()
+            student = session.query(StudentModel).filter(StudentModel.first_name == first_name).first()
+            student.first_name = new_name["first_name"]
+            student.last_name = new_name["last_name"]
+            session.commit()
+            return student.first_name
+        return "Invalid data"
 
-    # def delete(self, num):
-    #     del student[num]
-    #     return student
+    def delete(self):
+        if request.args.get("first_name") and request.args.get("last_name"):
+            first_name = request.args.get("first_name")
+            last_name = request.args.get("last_name")
+            session = get_session()
+            student = session.query(StudentModel).filter(StudentModel.first_name == first_name).first()
+            session.delete(student)
+            session.commit()
+            return student.first_name
+        return "Invalid data"
