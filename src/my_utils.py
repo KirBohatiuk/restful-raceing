c7aaef5779ffbcfc56d04fa2505bee81b68effec
kir <kirillbogatuk2005@gmail.com>
2023-12-29 20:25:29 +0200

    refactored a bit

--- src/my_utils.py
+++ src/my_utils.py
@@ -2,4 +2,8 @@
 
 
 
-def stutent_dict():
+def stutent_dict(students_list, first_name, last_name):
+    student_dict = {}
+    for i, student in enumerate(students_list):
+        student_dict[i] = {"first_name": first_name, "last_name": last_name}
+    return student_dict
--- src/view.py
+++ src/view.py
@@ -1,19 +1,16 @@
 from flask_restful import Resource
 from flask import request
 from .db import create_student, get_session, StudentModel
-
+from . import my_utils
 
 class StudentInfo(Resource):
     def get(self):
         if request.args.get("first_name") and request.args.get("last_name"):
             first_name = request.args.get("first_name")
             last_name = request.args.get("last_name")
-            student_dict = {}
-
             session = get_session()
             students = session.query(StudentModel).filter(StudentModel.first_name == first_name).all()
-            for i, student in enumerate(students):
-                student_dict[i] = {"first_name": first_name, "last_name": last_name}
+            student_dict = my_utils.stutent_dict(students, first_name, last_name)
             return student_dict
         return "Invalid data"
 
